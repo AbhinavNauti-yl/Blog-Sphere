b97c79bba6ed732551d8f3762834058780167b3a
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-14 16:39:41 +0530

    fix: remove unused import for Provider in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { Provider } from 'react-redux'
 import dotenv from "dotenv"
 dotenv.config()
 
