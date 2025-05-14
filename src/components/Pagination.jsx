6b1c8dd23a911aa87b7e7905cde7411d86f2d4fd
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-15 01:28:55 +0530

    fix: handle potential undefined paginationRange in Pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -9,7 +9,6 @@ export default function Pagination({
   totalPageCount,
 }) {
   const paginationRange = usePagination({
-      // siblingCount,
     currentPage,
     siblingCount,
     totalPageCount,
@@ -26,7 +25,7 @@ export default function Pagination({
     onPageChange(currentPage - 1);
   };
 
-  let lastPage = paginationRange[paginationRange.length - 1];
+  let lastPage = paginationRange[paginationRange?.length - 1];
 
   return (
     <div className="flex flex-col items-center px-5 py-5 bg-white xs:flex-row xs:justify-between">
@@ -48,7 +47,7 @@ export default function Pagination({
             <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z"></path>
           </svg>
         </button>
-        {paginationRange.map((pageNumber) => {
+        {paginationRange?.map((pageNumber) => {
           if (pageNumber === DOTS) {
             return (
               <button className="cursor-default w-full px-4 py-2 text-base bg-white border">
