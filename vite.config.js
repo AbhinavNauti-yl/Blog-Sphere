0276a289630bd0420beedfbb746b56b38b0753ab
AbhinavNauti-yl <abhinavnautiyal2004@gmail.com>
2025-05-14 14:52:06 +0530

    fix: update proxy configuration in package.json and vite.config.js to use a consistent backend URL

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.BASE_URL,
+        target: "https://blog-sphere-backend.onrender.com",
       },
     },
   },
